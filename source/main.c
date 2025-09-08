6e527d10cbd865370575e4b54fc80c5562ab533f
Ikxi <ikxi@ikxi.art>
2025-09-08 21:21:21 +0200

    i realized that arrow_down should probably move the cursor instead of advancing the text

--- source/main.c
+++ source/main.c
@@ -1,151 +1,186 @@
-#include <pokkenizer.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdint.h>
-#include <ncurses.h>
+    #include <pokkenizer.h>
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+    #include <unistd.h>
+    #include <stdint.h>
+    #include <ncurses.h>
 
 
-typedef struct double_link_list{
-    struct double_link_list *previous;
-    struct double_link_list *next;
-    uint8_t line_length;
-    char line[];
-} DoubleLinkList;
+    typedef struct double_link_list{
+        struct double_link_list *previous;
+        struct double_link_list *next;
+        uint8_t line_length;
+        char line[];
+    } DoubleLinkList;
 
 
-typedef struct cursor{
-    DoubleLinkList *row;
-    uint8_t col;
-} Cursor;
+    typedef struct cursor{
+        DoubleLinkList *row;
+        uint8_t col;
+    } Cursor;
 
 
-void free_memory(DoubleLinkList *head) {
-    DoubleLinkList *next = head;
-    while (next->next != NULL) {
-        DoubleLinkList *now = next;
-        next = next->next;
-        free(now);
-    }
-}
-
-
-int main(const int argc, char **argv) {
-    if (argc < 2) {printf("Provide a file name."); return 1;}
-    /*
-    Customized Pokkenizer, the persistent delimiters stays with the token.
-    It splits after the delimiter.
-    Tabs get converted to spaces.
-    */
-    char *token_buffer = get_pokke_buffer(argv[1], "", "\n");
-    size_t line_length = strlen_asm(token_buffer);
-    if (line_length > 200) {printf("Crossed the line length limit at line 0."); return 2;}
-    DoubleLinkList *head = calloc(1, sizeof(DoubleLinkList) + line_length);
-    head->line_length = line_length;
-    DoubleLinkList *previous = head;
-    DoubleLinkList *next;
-    memcpy(previous->line, token_buffer, line_length);
-    token_buffer += line_length;
-
-    int line_counter = 1;
-    while (*token_buffer != '\0') {
-        line_counter++;
-        if (*token_buffer == 10) {
-            // handle new line
-            if (*(token_buffer+2) != 10) {
-                line_counter--;
-                if (*(token_buffer+2) == 0) line_counter++; // handle EOF
-            }
-        }
-        line_length = strlen_asm(token_buffer); // add one for \0
-        if (line_length > 200) {printf("Crossed the line length limit at line %d.", line_counter); return 2;}
-
-        next = calloc(1, sizeof(DoubleLinkList) + line_length);
-        memcpy(next->line, token_buffer, line_length);
-        next->line_length = line_length;
-        previous->next = next;
-        next->previous = previous;
-        previous = next;
-        token_buffer += line_length;
-    }
-    moommap();
-
-    uint16_t row, col; // store number of rows and columns of the terminal
-    uint8_t current_col = 0; // for left-right scrolling
-    initscr();
-    raw();
-    noecho();
-    keypad(stdscr, TRUE);
-    nonl(); // no new line, Enter key is handled differently
-    #define ctrl(x)  ((x) & 0x1f) // macro to get value for Ctrl + <key>
-    int key;
-    DoubleLinkList *top;
-    top = head;
-    Cursor cursor = {top, 0};
-    uint8_t running = 1;
-
-    while (running == 1) {
-        getmaxyx(stdscr, row, col); // get rows and columns, make it resizable
-        next = top;
-        for (int i = 0; i < row-2;) {
-            if (next->line_length >= current_col)
-                mvprintw(i, 0, "%s", next->line+current_col);
-            else mvprintw(i, 0, "%s", next->line+next->line_length);
+    void free_memory(DoubleLinkList *head) {
+        DoubleLinkList *next = head;
+        while (next->next != NULL) {
+            DoubleLinkList *now = next;
             next = next->next;
-            if (next == 0) break;
-            ++i;
+            free(now);
         }
-
-        key = wgetch(stdscr);
-        mvprintw(row-1, 0, "                                ");
-        switch (key) {
-            case KEY_ENTER:
-                mvprintw(row-1, 0, "key: %d", key);
-                break;
-            case ctrl('c'):
-                mvprintw(row-1, 0, "key: ctrl c");
-                break;
-            case ctrl('q'):
-                printf("Goodbye\n");
-                running = 0;
-            case ctrl('j'):
-                mvprintw(row-1, 0, "key: ctrl j");
-                break;
-            case ctrl('f'):
-                mvprintw(row-1, 0, "key: ctrl f");
-                break;
-            case 258:
-                if (top->next != 0) top = top->next; break; // check EOF
-            case 259:
-                if (top->previous != 0) top = top->previous; break; // check BOF
-            case 338:
-                for (int i = 0; i < row -3;) {
-                    if (top->next != 0) top = top->next;
-                    else if (top->next == 0) break;
-                    ++i;
-                }
-                break;
-            case 339:
-                for (int i = 0; i < row -3;) {
-                    if (top->previous != 0) top = top->previous;
-                    else if (top->previous == 0) break;
-                    ++i;
-                }
-                break;
-            case 555:
-                if (current_col > 0) current_col--; break;
-            case 570:
-                if (current_col < 200) current_col++; break;
-            default:
-                mvprintw(row-1, 0, "key: %d", key);
-                break;
-        }
-
-        refresh();
     }
 
-    endwin();
-    free_memory(head);
-    return 0;
-}
+
+    int main(const int argc, char **argv) {
+        if (argc < 2) {printf("Provide a file name."); return 1;}
+        /*
+        Customized Pokkenizer, the persistent delimiters stays with the token.
+        It splits after the delimiter.
+        Tabs get converted to spaces.
+        */
+        char *token_buffer = get_pokke_buffer(argv[1], "", "\n");
+        size_t line_length = strlen_asm(token_buffer);
+        if (line_length > 200) {printf("Crossed the line length limit at line 0."); return 2;}
+        DoubleLinkList *head = calloc(1, sizeof(DoubleLinkList) + line_length);
+        head->line_length = line_length;
+        DoubleLinkList *previous = head;
+        DoubleLinkList *next;
+        memcpy(previous->line, token_buffer, line_length);
+        token_buffer += line_length;
+
+        int line_counter = 1;
+        while (*token_buffer != '\0') {
+            line_counter++;
+            if (*token_buffer == 10) {
+                // handle new line
+                if (*(token_buffer+2) != 10) {
+                    line_counter--;
+                    if (*(token_buffer+2) == 0) line_counter++; // handle EOF
+                }
+            }
+            line_length = strlen_asm(token_buffer); // add one for \0
+            if (line_length > 200) {printf("Crossed the line length limit at line %d.", line_counter); return 2;}
+
+            next = calloc(1, sizeof(DoubleLinkList) + line_length);
+            memcpy(next->line, token_buffer, line_length);
+            next->line_length = line_length;
+            previous->next = next;
+            next->previous = previous;
+            previous = next;
+            token_buffer += line_length;
+        }
+        moommap();
+
+        uint16_t row, col; // store number of rows and columns of the terminal
+        uint8_t current_col = 0; // for left-right scrolling
+        initscr();
+        raw();
+        noecho();
+        keypad(stdscr, TRUE);
+        nonl(); // no new line, Enter key is handled differently
+        #define ctrl(x)  ((x) & 0x1f) // macro to get value for Ctrl + <key>
+        int key;
+        DoubleLinkList *top;
+        top = head;
+        Cursor cursor = {top, 0};
+        uint8_t running = 1;
+
+        while (running == 1) {
+            // logic
+            getmaxyx(stdscr, row, col); // get rows and columns, make it resizable
+            next = top;
+
+            key = wgetch(stdscr);
+            mvprintw(row-1, 0, "                                ");
+            switch (key) {
+                case ctrl('q'): // quit
+                    printf("Goodbye\n");
+                    running = 0;
+
+                case KEY_ENTER:
+                    mvprintw(row-1, 0, "key: %d", key);
+                    break;
+
+                case 258: // arrow_down
+                    if (cursor->row->next != 0) { // check EOF
+                        cursor->row = cursor->row->next;
+                        if ()
+                            top = top->next;
+                        mvprintw(row-1, 0, "arrow_down");
+                        break;
+                    }
+                    mvprintw(row-1, 0, "arrow_down reached end of file");
+                    break;
+
+                case 259: // arrow_up
+                    if (top->previous != 0) { // check BOF
+                        top = top->previous;
+                        mvprintw(row-1, 0, "arrow_up");
+                        break;
+                    }
+                    mvprintw(row-1, 0, "arrow_up reached beginning of file");
+                    break;
+
+                case 338: // page down
+                    mvprintw(row-1, 0, "key: %d", key);
+                    for (int i = 0; i < row -3;) {
+                        if (top->next != 0) top = top->next;
+                        else if (top->next == 0) break;
+                        ++i;
+                    }
+                    break;
+
+                case 339: // page up
+                    mvprintw(row-1, 0, "key: %d", key);
+                    for (int i = 0; i < row -3;) {
+                        if (top->previous != 0) top = top->previous;
+                        else if (top->previous == 0) break;
+                        ++i;
+                    }
+                    break;
+
+                case 555: // alt left_arrow
+                    mvprintw(row-1, 0, "key: %d", key);
+                    if (current_col > 0) current_col--; break;
+
+                case 570: // alt right_arrow
+                    mvprintw(row-1, 0, "key: %d", key);
+                    if (current_col < 200) current_col++; break;
+
+                case ctrl('c'): // unhandled
+                    mvprintw(row-1, 0, "key: ctrl c");
+                    break;
+
+                case ctrl('j'): // unhandled
+                    mvprintw(row-1, 0, "ctrl j");
+                    break;
+
+                case ctrl('f'): // unhandled
+                    mvprintw(row-1, 0, "ctrl f");
+                    break;
+
+                default:
+                    mvprintw(row-1, 0, "key: %d", key);
+                    break;
+            }
+
+            // graphics
+            for (int i = 0; i < row-2;) {
+                if (next == cursor->row) { // cursor printing
+
+                }
+                if (next->line_length >= current_col)
+                    mvprintw(i, 0, "%s", next->line+current_col);
+                else mvprintw(i, 0, "%s", next->line+next->line_length);
+                next = next->next;
+                if (next == 0) break;
+                ++i;
+            }
+
+            refresh();
+        }
+
+        endwin();
+        free_memory(head);
+        return 0;
+    }
